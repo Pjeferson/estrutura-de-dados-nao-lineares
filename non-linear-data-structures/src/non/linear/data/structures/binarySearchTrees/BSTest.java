87b605287dba22f54c9ab5f176a3873807b82458
Pjeferson <paulojeferson34@gmail.com>
2018-08-20 21:15:03 -0300

    Testing all remove ways

--- non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/BSTest.java
+++ non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/BSTest.java
@@ -11,11 +11,22 @@ package non.linear.data.structures.binarySearchTrees;
  */
 public class BSTest {
     public static void main(String[] args) {
-        // BinarySearchTree bst;
         BinarySearchTreeDefault bst = new BinarySearchTreeDefault();
+        bst.insert(30, 30);
         bst.insert(10, 10);
+        bst.insert(40, 40);
         bst.insert(20, 20);
+        bst.insert(31, 31);
+        bst.insert(11, 11);
+        bst.insert(41, 41);
+        bst.insert(21, 21);
         bst.order();
+        bst.remove(30);
+        bst.remove(40);
+        bst.remove(31);
+        bst.order();
+
         System.out.println(bst.size());
+        System.out.println(bst.find(40) != null);
     }
 }
