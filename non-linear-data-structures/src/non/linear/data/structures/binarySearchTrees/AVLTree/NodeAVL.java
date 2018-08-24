c447258b576d6251571de93fbdf698cdf7837af5
Pjeferson <paulojeferson34@gmail.com>
2018-08-23 21:15:33 -0300

    adding a way to print the nodes

--- non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/AVLTree/NodeAVL.java
+++ non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/AVLTree/NodeAVL.java
@@ -45,7 +45,9 @@ public class NodeAVL {
     public void setVal(Object val) {
         this.val = val;
     }
-
+    public boolean hasParent() {
+        return (this.getParent()!= null);
+    }
     public NodeAVL getParent() {
         return parent;
     }
@@ -53,7 +55,9 @@ public class NodeAVL {
     public void setParent(NodeAVL parent) {
         this.parent = parent;
     }
-
+    public boolean isLeft(NodeAVL other) {
+        return (this.getLeft() != null && this.getLeft().getKey() == other.getKey());
+    }
     public NodeAVL getLeft() {
         return left;
     }
@@ -61,7 +65,9 @@ public class NodeAVL {
     public void setLeft(NodeAVL left) {
         this.left = left;
     }
-
+    public boolean isRight(NodeAVL other) {
+        return (this.getRight() != null && this.getRight().getKey() == other.getKey());
+    }
     public NodeAVL getRight() {
         return right;
     }
@@ -82,5 +88,36 @@ public class NodeAVL {
     }
     public void decFb() {
         this.fb--;
+    }
+    
+    public void printTree(StringBuffer out){
+        if (right != null) {
+            right.printTree(out, true, "");
+        }
+        printNodeValue(out);
+        if (left != null) {
+            left.printTree(out, false, "");
+        }
+    }
+    private void printNodeValue(StringBuffer out){
+        out.append(key+":"+fb);
+        out.append('\n');
+    }
+    // use string and not stringbuffer on purpose as we need to change the indent at each recursion
+    private void printTree(StringBuffer out, boolean isRight, String indent){
+        if (right != null) {
+            right.printTree(out, true, indent + (isRight ? "        " : " |      "));
+        }
+        out.append(indent);
+        if (isRight) {
+            out.append(" /");
+        } else {
+            out.append(" \\");
+        }
+        out.append("----- ");
+        printNodeValue(out);
+        if (left != null) {
+            left.printTree(out, false, indent + (isRight ? " |      " : "        "));
+        }
     }
 }
