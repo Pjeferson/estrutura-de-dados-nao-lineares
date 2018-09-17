e283c8c0ac9856e3398c40d7ea71316caef9959c
Pjeferson <paulojeferson34@gmail.com>
2018-09-17 16:24:22 -0300

    Defining tree structure / Implementing the insert method

--- non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/TreeRB.java
+++ non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/TreeRB.java
@@ -10,5 +10,65 @@ package non.linear.data.structures.binarySearchTrees.RedBlackTree;
  * @author pjeferson
  */
 public class TreeRB {
+    private NodeRB root,nil;
+    private int size;
     
+    public TreeRB () {
+        this.root = null;
+        this.size = 0;
+        this.nil = new NodeRB(-1);
+        if(!this.nil.isBlack()) this.nil.changeColor();
+    }
+    
+    public void insert(int key) {
+        NodeRB n = new NodeRB(key);
+        if(this.root == null) { // First Node
+            this.root = n;
+            this.size = 1;
+            n.setParent(this.nil);
+            n.setLeft(this.nil);
+            n.setRight(this.nil);
+            return;
+        }
+        insert(this.root, n);
+        //updateFbInsert(inserted);
+        this.size++;
+    }
+    private void insert(NodeRB current, NodeRB inserting) {
+        if (inserting.getKey() < current.getKey()){
+            if(current.getLeft() == this.nil){
+                current.setLeft(inserting);
+                inserting.setParent(current);
+                inserting.setLeft(this.nil);
+                inserting.setRight(this.nil);
+                
+            }
+            else {
+                insert(current.getLeft(), inserting);
+            }
+        } else {
+            if(current.getRight() == this.nil){
+                current.setRight(inserting);
+                inserting.setParent(current);
+                inserting.setLeft(this.nil);
+                inserting.setRight(this.nil);
+            }
+            else {   
+                insert(current.getRight(), inserting);
+            }
+        }
+    }
+    
+    public void order(){
+        order(this.root);
+        System.out.print("\n");
+    }
+    private void order(NodeRB n){
+        if(n == this.nil) return;
+        else {
+            order(n.getLeft());
+            System.out.print(n.getKey() + " ");
+            order(n.getRight());
+        }
+    }
 }
