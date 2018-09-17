e9ff2be3967b269ed5f25b732747b5b1d60007c2
Pjeferson <paulojeferson34@gmail.com>
2018-09-17 16:23:02 -0300

    Defining the RB-Tree node structure

--- non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/NodeRB.java
+++ non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/NodeRB.java
@@ -10,5 +10,53 @@ package non.linear.data.structures.binarySearchTrees.RedBlackTree;
  * @author pjeferson
  */
 public class NodeRB {
+    private int key;
+    private boolean color;/*0-Red, 1-Black*/
     
+    private NodeRB parent, left,right;
+    
+    public NodeRB(int key) {
+        this.key = key;
+        this.color = false;
+    }
+    
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public boolean isBlack() {
+        return color;
+    }
+
+    public void changeColor() {
+        this.color = !this.color;
+    }
+
+    public NodeRB getParent() {
+        return parent;
+    }
+
+    public void setParent(NodeRB parent) {
+        this.parent = parent;
+    }
+
+    public NodeRB getLeft() {
+        return left;
+    }
+
+    public void setLeft(NodeRB left) {
+        this.left = left;
+    }
+
+    public NodeRB getRight() {
+        return right;
+    }
+
+    public void setRight(NodeRB right) {
+        this.right = right;
+    }
 }
