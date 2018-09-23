c800e6b1210dcaf76491102fc7327b3c6dee669c
Pjeferson <paulojeferson34@gmail.com>
2018-09-23 15:45:28 -0300

    Testing the RB-Tree interactively

--- non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/TestRB.java
+++ non-linear-data-structures/src/non/linear/data/structures/binarySearchTrees/RedBlackTree/TestRB.java
@@ -5,18 +5,44 @@
  */
 package non.linear.data.structures.binarySearchTrees.RedBlackTree;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pjeferson
  */
 public class TestRB {
     public static void main(String[] args) {
-       TreeRB rb = new TreeRB();
-       rb.insert(4);
+        TreeRB rb = new TreeRB();
+        
+        boolean key = true;
+        Scanner reader = new Scanner(System.in);
+        while(key){
+            System.out.println("Ops: in, rm and end");
+            String in = reader.next();
+            int n;
+            switch(in){
+               case "in":
+                    n = reader.nextInt();
+                    rb.insert(n);
+                    break;
+               case "rm":
+                    n = reader.nextInt();
+                    rb.remove(n);
+                    break;
+               case "end":
+                    key = false;
+                    reader.close();
+                    break;
+            }
+            rb.print();
+        }
+        
+       /*rb.insert(4);
        rb.insert(26);
        rb.insert(3);
        rb.insert(9);
-       rb.insert(15);
+       rb.insert(15);*/
        
        rb.print();
        rb.order();
